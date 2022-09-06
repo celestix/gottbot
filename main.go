29b1e58a1c25736a39fc6203f54f6b18f1818bcf
anonyindian <anony@ferrisgram.org>
2022-09-06 20:16:57 +0530

    f

--- main.go
+++ main.go
@@ -1,19 +1,39 @@
 package main
 
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+)
+
+type Schemas struct {
+	Properties Properties `json:"properties,omitempty"`
+	Required   []string   `json:"required,omitempty"`
+}
+type Properties struct {
+	Description any `json:"description,omitempty"`
+	Type        any `json:"type"`
+	Format      any `json:"format,omitempty"`
+	Nullable    any `json:"nullable,omitempty"`
+}
+
+type Components struct {
+	Schemas map[string]Schemas `json:"schemas"`
+}
+
 type API struct {
-	Components struct {
-		Schemas map[string]struct {
-			Properties struct {
-				Description string `json:"description"`
-				Type        string `json:"type"`
-				Format      string `json:"format,omitempty"`
-			} `json:"properties,omitempty"`
-			Required []string `json:"required,omitempty"`
-		} `json:"schemas"`
-	} `json:"components"`
-	Paths any `json:"paths"`
+	Components Components `json:"components"`
+	// Paths any `json:"paths"`
 }
 
 func main() {
-
+	b, err := ioutil.ReadFile("schema.json")
+	if err != nil {
+		panic(err.Error())
+	}
+	var api API
+	if err = json.Unmarshal(b, &api); err != nil {
+		panic(err.Error())
+	}
+	fmt.Println(api)
 }
